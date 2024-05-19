eb8c84344d4787d23adad8ac6dd7f69c958afcbf
Derourn <96676237+DeroXP@users.noreply.github.com>
2024-05-19 02:24:43 -0500

    Update simple.js

--- simple.js
+++ simple.js
@@ -1,4 +1,27 @@
 // this uses cookie monster to get best options and then simulates clicking best option and also does automactic cookie clicks which cannot make it go faster, since idk
+// Throttle function to limit how often a function can be called
+function throttle(func, limit) {
+    let inThrottle;
+    return function() {
+        const args = arguments;
+        const context = this;
+        if (!inThrottle) {
+            func.apply(context, args);
+            inThrottle = true;
+            setTimeout(() => inThrottle = false, limit);
+        }
+    };
+}
+
+const bigCookie = document.getElementById("bigCookie");
+let productElements = [];
+for (let i = 0; document.getElementById(`product${i}`); i++) {
+    productElements.push(document.getElementById(`product${i}`));
+}
+
+const throttledClickBigCookie = throttle(() => simulateClick(bigCookie), 100);
+const throttledCheckAndClickProducts = throttle(checkAndClickProducts, 1000);
+
 function simulateClick(element) {
     if (element) {
         element.click();
@@ -6,11 +29,9 @@ function simulateClick(element) {
 }
 
 function checkAndClickProductsByColor(color) {
-    let index = 0;
-    let productElement = document.getElementById(`product${index}`);
     let bestProduct = null;
 
-    while (productElement) {
+    for (let productElement of productElements) {
         if (productElement.classList.contains("unlocked") && productElement.classList.contains("disabled")) {
             let priceElement = productElement.querySelector('.price');
 
@@ -18,9 +39,6 @@ function checkAndClickProductsByColor(color) {
                 bestProduct = productElement;
             }
         }
-
-        index++;
-        productElement = document.getElementById(`product${index}`);
     }
 
     if (bestProduct && bestProduct.classList.contains("enabled")) {
@@ -48,15 +66,12 @@ function checkAndClickProducts() {
 }
 
 function clickBigCookie() {
-    const bigCookie = document.evaluate('//*[@id="bigCookie"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-    simulateClick(bigCookie);
+    throttledClickBigCookie();
 }
 
 document.addEventListener("click", function() {
-    setInterval(checkAndClickProducts, 100);
-
-    const clickInterval = 1;
-    setInterval(clickBigCookie, clickInterval);
+    setInterval(throttledCheckAndClickProducts, 1000);
+    setInterval(throttledClickBigCookie, 100);
 });
 
 javascript: (function () {   Game.LoadMod('https://cookiemonsterteam.github.io/CookieMonster/dist/CookieMonster.js'); })();
