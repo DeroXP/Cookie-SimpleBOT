7a308b35c02272a6811933ba7aec631db38092e9
Derourn <96676237+DeroXP@users.noreply.github.com>
2024-05-19 12:00:18 -0500

    Update simple.js

--- simple.js
+++ simple.js
@@ -1,5 +1,5 @@
 // this uses cookie monster to get best options and then simulates clicking best option and also does automactic cookie clicks which cannot make it go faster, since idk
-// Function to limit how often a function can be called
+// function to limit how often a function can be called
 function throttle(func, limit) {
     let inThrottle;
     return function() {
@@ -13,12 +13,6 @@ function throttle(func, limit) {
     };
 }
 
-function simulateClick(element) {
-    if (element) {
-        element.click();
-    }
-}
-
 const bigCookie = document.getElementById("bigCookie");
 let productElements = [];
 for (let i = 0; document.getElementById(`product${i}`); i++) {
@@ -28,36 +22,51 @@ for (let i = 0; document.getElementById(`product${i}`); i++) {
 const throttledClickBigCookie = throttle(() => simulateClick(bigCookie), 100);
 const throttledCheckAndClickProducts = throttle(checkAndClickProducts, 1000);
 
+function simulateClick(element) {
+    if (element) {
+        element.click();
+    }
+}
+
 const colors = ['rgb(0, 255, 0)', 'rgb(255, 255, 0)', 'rgb(255, 127, 0)', 'rgb(255, 0, 0)'];
 
 function checkAndClickProductsByColor() {
+    let found = false;
     for (let color of colors) {
+        let bestProduct = null;
+
         for (let productElement of productElements) {
             if (productElement.classList.contains("unlocked")) {
                 let priceElement = productElement.querySelector('.price');
 
                 if (priceElement && priceElement.style.color === color) {
-                    if (productElement.classList.contains("enabled")) {
-                        simulateClick(productElement);
-                        return true;
-                    } else {
-                        let intervalId = setInterval(() => {
-                            if (productElement.classList.contains("enabled")) {
-                                simulateClick(productElement);
-                                clearInterval(intervalId);
-                            }
-                        }, 1000);
-                        return true;
+                    if (!bestProduct || parseInt(priceElement.textContent.replace(/,/g, '')) < parseInt(bestProduct.querySelector('.price').textContent.replace(/,/g, ''))) {
+                        bestProduct = productElement;
                     }
                 }
             }
         }
+
+        if (bestProduct && bestProduct.classList.contains("enabled")) {
+            simulateClick(bestProduct);
+            found = true;
+        } else if (bestProduct && bestProduct.classList.contains("disabled")) {
+            var checkExist = setInterval(function() {
+                if (bestProduct.classList.contains("enabled")) {
+                    simulateClick(bestProduct);
+                    found = true;
+                    clearInterval(checkExist);
+                }
+            }, 1000);
+        }
     }
-    return false;
+
+    return found;
 }
 
 function checkAndClickProducts() {
     let found = checkAndClickProductsByColor();
+
     if (!found) {
         setTimeout(checkAndClickProducts, 1000);
     }
@@ -67,11 +76,9 @@ function clickBigCookie() {
     throttledClickBigCookie();
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    setInterval(throttledCheckAndClickProducts, 1000);
-    setInterval(throttledClickBigCookie, 100);
+document.addEventListener("click", function() {
+    setInterval(throttledCheckAndClickProducts, 5000);
+    setInterval(throttledClickBigCookie, 1000);
 });
 
-(function () { 
-    Game.LoadMod('https://cookiemonsterteam.github.io/CookieMonster/dist/CookieMonster.js'); 
-})();
+javascript: (function () {   Game.LoadMod('https://cookiemonsterteam.github.io/CookieMonster/dist/CookieMonster.js'); })();
